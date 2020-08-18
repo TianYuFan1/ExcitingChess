7e15d244a3b4eefde64f673d2054b848f7abaabe
TianYuFan1 <tianyufan01@gmail.com>
2020-08-18 14:53:19 -0400

    create match board setup

--- src/main/java/edu/wpi/teamname/views/match/MatchBoardController.java
+++ src/main/java/edu/wpi/teamname/views/match/MatchBoardController.java
@@ -1,3 +1,15 @@
 package edu.wpi.teamname.views.match;
 
-public class MatchBoardController {}
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class MatchBoardController {
+    @FXML AnchorPane pn_chat;
+    @FXML AnchorPane pn_moves;
+    @FXML AnchorPane pn_board;
+    @FXML AnchorPane pn_controls;
+
+    public void initialize() {
+
+    }
+}
